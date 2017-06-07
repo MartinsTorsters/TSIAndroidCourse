7cf74f8ff3d4e9080b89afe463fe549e4ebeecaf
Mazzeri <martins.mazzeri@gmail.com>
2017-06-07 23:43:31 +0200

    Adding firebase (writting is failing)

--- MyApplication/app/src/main/java/com/example/martins/tsiapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/martins/tsiapplication/MainActivity.java
@@ -1,27 +1,74 @@
 package com.example.martins.tsiapplication;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.example.martins.tsiapplication.models.TaskDO;
+import com.firebase.ui.database.FirebaseListAdapter;
+import com.google.firebase.FirebaseApiNotAvailableException;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
+    FirebaseDatabase database;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        try {
+            FirebaseApp.initializeApp(this);
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+            setSupportActionBar(toolbar);
 
+            final ListView listview = (ListView) findViewById(R.id.to_do_list);
+
+            database = FirebaseDatabase.getInstance();
+
+            final DatabaseReference taskRef = database.getReference("tasks");
+            final FirebaseListAdapter<TaskDO> myAdapter = new FirebaseListAdapter<TaskDO>(this, TaskDO.class, R.layout.activity_main, taskRef) {
+                @Override
+                protected void populateView(View v, TaskDO model, int position) {
+                    final TextView titleTextView = (TextView) findViewById(R.id.to_do_title);
+                    titleTextView.setText(model.getName());
+                    final TextView subtitleTextView = (TextView) findViewById(R.id.to_do_description);
+                    subtitleTextView.setText(model.getDetails());
+                    final TextView detailTextView = (TextView) findViewById(R.id.to_do_detail);
+                    detailTextView.setText(model.getUrl());
+                    final ImageView thumbnailImageView = (ImageView) findViewById(R.id.to_do_thumbnail);
+                    thumbnailImageView.setImageURI(Uri.parse(model.getUrl()));
+                }
+            };
+            listview.setAdapter(myAdapter);
+            addFloatingActionButton();
+        } catch (Exception e) {
+            Log.w("wtf", e);
+            System.out.print(e.getLocalizedMessage());
+        }
+    }
+
+    private void addFloatingActionButton() {
         final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                writeNewTask("jfb", "odnfig", "https://www.syndicatdermatos.org/wp-content/uploads/2015/08/autocollant-tete-maure-corse.jpg", "");
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
@@ -33,6 +80,11 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
     }
 
     @Override
@@ -48,5 +100,16 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void writeNewTask(String title, String details, String url, String label) {
+        String key = database.getReference().child("tasks").push().getKey();
+        final TaskDO task = new TaskDO(key, title, details, url);
+        Map<String, Object> postValues = task.toMap();
+
+        Map<String, Object> childUpdates = new HashMap<>();
+        childUpdates.put("/tasks/" + key, postValues);
+
+        database.getReference().updateChildren(childUpdates);
     }
 }
--- MyApplication/app/src/main/java/com/example/martins/tsiapplication/models/TaskDO.java
+++ MyApplication/app/src/main/java/com/example/martins/tsiapplication/models/TaskDO.java
@@ -1,4 +1,124 @@
 package com.example.martins.tsiapplication.models;
 
+import com.google.firebase.database.Exclude;
+import com.google.firebase.database.IgnoreExtraProperties;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@IgnoreExtraProperties
 public class TaskDO {
+
+    private String name;
+    private String details;
+    private Date toDoDate;
+    private String id;
+    private String label;
+    private String url;
+
+    public TaskDO() {
+    }
+
+    public TaskDO(String id, String name, String details, String url) {
+        this.name = name;
+        this.details = details;
+        this.url = url;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public Date getToDoDate() {
+        return toDoDate;
+    }
+
+    public void setToDoDate(Date toDoDate) {
+        this.toDoDate = toDoDate;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        TaskDO taskDO = (TaskDO) o;
+
+        if (name != null ? !name.equals(taskDO.name) : taskDO.name != null) return false;
+        if (details != null ? !details.equals(taskDO.details) : taskDO.details != null)
+            return false;
+        if (toDoDate != null ? !toDoDate.equals(taskDO.toDoDate) : taskDO.toDoDate != null)
+            return false;
+        return id != null ? id.equals(taskDO.id) : taskDO.id == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + (details != null ? details.hashCode() : 0);
+        result = 31 * result + (toDoDate != null ? toDoDate.hashCode() : 0);
+        result = 31 * result + (id != null ? id.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "TaskDO{" +
+                "name='" + name + '\'' +
+                ", details='" + details + '\'' +
+                ", toDoDate=" + toDoDate +
+                ", id=" + id +
+                '}';
+    }
+
+    @Exclude
+    public Map<String, Object> toMap() {
+        final HashMap<String, Object> result = new HashMap<>();
+        result.put("id", id);
+        result.put("title", name);
+        result.put("details", details);
+        result.put("url", url);
+        result.put("label", label);
+
+        return result;
+    }
 }
